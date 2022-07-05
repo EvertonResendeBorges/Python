07630701f3f1e001c68eac9ebee09c39c3484f80
Everton Resende Borges <evertonrborges12@gmail.com>
2022-07-05 15:10:19 -0300

    Exercicios de Python

---
+++ pro.py
@@ -0,0 +1,16 @@
+print ("Oi mundo!")
+nome = input("Qual é o seu nome?")
+print (nome)
+print ("Seu nome é ",nome)
+print (f"Seu nome com format é {nome}")
+
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+
+print(dia,"/",mes,"/",ano)
+
+n1 = input()
+n2 = input()
+
+print(n1 + n2)
